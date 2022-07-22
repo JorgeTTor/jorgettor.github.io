887ffc85c474f6d09c51f1531791fec1ccb7f250
Jorge.T.T <71452849+JorgeTTor@users.noreply.github.com>
2022-07-22 16:13:55 -0500

    Updates

---
+++ assets/main.js
@@ -0,0 +1,49 @@
+const API = 'https://youtube-v31.p.rapidapi.com/search?channelId=UC1emV4A8liRs9p80CY8ElUQ&part=snippet%2Cid&order=date&maxResults=9';
+
+const content = null || document.getElementById('content');
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e030d74196mshdb4f955e0a9c86bp15f87ajsn751f244228e6',
+		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
+	}
+};
+
+async function fetchData(urlApi){
+  const response = await fetch(urlApi, options);
+  const data = await response.json();
+  return data;
+}
+
+(async () => {
+  try {
+    const videos = await fetchData(API);
+    let view = `
+    ${videos.items.map(video =>  `
+    <div class="group relative">
+      <div
+        class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+      </div>
+      <div class="mt-4 flex justify-between">
+        <h3 class="text-sm text-gray-700">
+          <span aria-hidden="true" class="absolute inset-0"></span>
+          ${video.snippet.title}
+        </h3>
+      </div>
+    </div>
+    `).slice(0,4).join('')}
+    
+    `;
+    content.innerHTML = view;
+  } catch (error) {
+    console.log(error);
+  }
+})();
+
+
+// fetch('', options)
+// 	.then(response => response.json())
+// 	.then(response => console.log(response))
+// 	.catch(err => console.error(err));
